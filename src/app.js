91fd1c5606d277e7780fb7c545ec66fe4bb90d3f
Jovanh228 <jeoaxel@gmail.com>
2024-12-15 19:57:32 -0600

    TERCERO

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ import seriesRoutes from './routes/series.routes.js'
 
 const app = express();
 app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:3000'],
+    origin: ['http://localhost:5173','http://localhost:3000',process.env.BASE_URL],
     credentials:true
 }));
 app.use(morgan('dev'));
